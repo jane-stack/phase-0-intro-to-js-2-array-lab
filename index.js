394f83ec360a527936c8f5338335e098ae80aaa2
Tuyen Ha <tuyen_ha@outlook.com>
2022-06-16 16:00:12 -0600

    commit tree 956ebfa23ca6e83c4f284c394d8db669428b331c

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat() {
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat() {
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat() {
+    const catsCopied = [...cats, "Broom"];
+    return catsCopied;
+    }
+
+function prependCat() {
+    const catsCopied = ["Arnold", ...cats];
+    return catsCopied;
+}
+
+function removeLastCat() {
+    const copiesOfCats = cats.slice(0, cats.length -1);
+    return copiesOfCats;
+}
+
+function removeFirstCat() {
+    let copiesOfCats = [...cats];
+    copiesOfCats.shift();
+    console.log(copiesOfCats);
+    return copiesOfCats;
+}
